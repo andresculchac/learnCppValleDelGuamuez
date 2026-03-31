541971ff9f903b9f664ebd33e482981171cef100
Andresito <mompyrri2001@gmail.com>
2026-03-31 16:50:03 -0500

    add tasks.json for C++ build configuration and create theGreeting.cpp with a simple hello world function

---
+++ theGreeting.cpp
@@ -0,0 +1,16 @@
+/* It's my first C++ function
+Date Today : 2026-03-31
+author : MOMPYLABS */
+#include <iostream>
+using namespace std;
+
+void sayHello() {
+    cout << "Hello, World!" << endl;
+
+}
+
+int main() {
+    sayHello();
+    
+    return 0;
+}
