cf659970361cb7221f1f28a7a528f8cf319c88f8
Andresito <mompyrri2001@gmail.com>
2026-03-31 16:29:56 -0500

    add findThesquare.cpp to calculate the square of a number

---
+++ findThesquare.cpp
@@ -0,0 +1,12 @@
+// date : 2026-03-31
+// author : MOMPYLABS
+
+#include <iostream>
+
+int main() { 
+    int numero{};
+    std::cin >> numero;
+    std::cout << numero * numero << '\n';
+    return 0;
+
+}
